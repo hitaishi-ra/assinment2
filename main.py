c314aee87be68865b8acbc379653a0f8088319d7
hitaishi-ra <71496870+hitaishi-ra@users.noreply.github.com>
2020-09-20 16:55:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+"""1)	In the Python statement x = a + 5 - b:
+	•	a and b are ________
+	•	a + 5 - b is ________
+	a)	operands, an equation
+	b)	operands, an expression
+	c)	operators, a statement
+	d)	terms, a group
+"""
+b ="operands,an expression"
+print(b)
+"""
+2)	Write a program to print the quotient when 9 is divided by 2.
+"""
+
+print(9/2)
+
+"""
+
+3)	Write a program to print the remainder when 22 is divided by 3.
+"""
+
+print(22%3)
+
+"""
+
+4)	Write a program to create two variables, store an integer value in them and run the arithmetic operations on them. Print the answers.
+"""
+p=(85)
+g=(98)
+print(p+g)
+print(p-g)
+print(p/g)
+print(p*g)
+print(p%g)
+print(p**g)
+
+
+"""
+
+5)	Write a program to create two variables, store an integer value in them and run the comparison/relational operations on them. Print the answers.
+"""
+p=(85)
+g=(98)
+print(p==g)
+print(p!=g)
+
+
+
+"""
+6)	Write a program to create two variables. Store any integer value in them and check:
+	a)	If both of them are divisible by 4 and 3. Print the answer.
+	b)	If both of them are not divisible by 4 and 3. Print the answer.
+	c)	If any one of them is divisible by 4 or 3. Print the answer.
+"""
+p=(85)
+g=(98)
+
+
+
+"""
+7)	Write a program to create two variables. Assign any value to them using assig operator. Once the values are assigned perform the assignment operations on them. Print the result.
+
+8)	How can one check if 10 is greater than 0 and less than 20? Write a program for the same.
+"""
+a=10
+print(a>0 and a<20)
+
+
+"""
+9)	Write a program to create three lists, list1, list2 and list3 containing the name of country, person and object respectively. Create another variable and assign it the value of any list of your choice from the three lists. Check if the new created list is equal to any of the list using identity operator. Also using membership operator find if the member of the list exist in that particular list or not? 
+
+"""
+list1=["india","japan","china"]
+list2=["boy","girl","lady"]
+list3=["bat","ball","hat"]
+o = list1
+print(list1 is o)
+print(list1 is not o)
+print(list2 is o)
+print(list3 is o)
+print("japan"in o)
+
+
+
+"""
+
+
+"""
+
